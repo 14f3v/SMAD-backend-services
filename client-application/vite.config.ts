f015e81f83504c385d2fa7d67c103229ea03b755
contain3rlab <svnps.rnd.team@gmail.com>
2024-02-19 12:34:11 +0700

    apply client react-app generated for client side simple showcase

---
+++ client-application/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/': "http://hyperscale-application-showcase:4431/"
+    }
+  }
+})
