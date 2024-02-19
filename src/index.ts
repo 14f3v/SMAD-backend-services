ce08b5d51c59c37a724d67c49545005e59ff6276
contain3rlab <svnps.rnd.team@gmail.com>
2024-02-19 17:05:34 +0700

    find out solution for connection failure between application and redis server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import Config from '@constants/configs';
 import SocketIOEmittion from '@constants/socketIOemittion';
 import { socketIO, server, app } from '@server';
+import RedisServices from '@services/redis';
 import DependenciesInjection from '@services/dependenciesInjection';
 import SocketIOService from '@services/socketIOService';
-export default (() => {
+const redisServices = RedisServices.getInstance();
+
+export default (async () => {
     server.listen(Config.APPLICATION_PORT, new DependenciesInjection().initInstance);
-    socketIO.sockets.on(SocketIOEmittion.CONNECT, (socket) => new SocketIOService(socket).initialize())
     return;
-})();
+})().then(async function () {
+    socketIO.sockets.on(SocketIOEmittion.CONNECT, (socket) => new SocketIOService(socket).initialize());
+    await redisServices.initialize().then(async (ref) => await redisServices.broadcastingNewApplicationInstanceMember());
+});
--- src/services/redis/index.ts
+++ src/services/redis/index.ts
@@ -1,8 +1,15 @@
 import Config from '@constants/configs';
+import RedisEmittion from '@constants/redisEmittion';
 import { createClient, type RedisClientType } from 'redis';
+import InitializePool from './InitializePools';
+import DependenciesInjection from '@services/dependenciesInjection';
 
 class RedisServices {
     private static instance: RedisServices;
+    private dependenciesInjection = DependenciesInjection.getInstance();
+    public connection: RedisClientType = createClient(redisConnection);
+    public subscriber?: RedisClientType/*  = this.connection.duplicate() */;
+    public publisher?: RedisClientType/*  = this.connection.duplicate() */;
     public static getInstance(): RedisServices {
         if (!RedisServices.instance) {
             RedisServices.instance = new RedisServices();
@@ -10,19 +17,43 @@ class RedisServices {
         return RedisServices.instance;
     };
 
-    public subscriber(): RedisClientType {
-        return createClient(redisConnection)
+    public jsonSerialize(context: any) {
+        if (typeof context == 'string') context = JSON.parse(context);
+        else context = JSON.stringify(context)
+
+        return context;
     };
 
-    public publisher(): RedisClientType {
-        return this.subscriber().duplicate(redisConnection)
+    public async broadcastingNewApplicationInstanceMember() {
+        await this.publisher?.publish(RedisEmittion.LISTEN_MESSAGE, this.jsonSerialize(this.dependenciesInjection.applicationDetail));
+        console.log(this.jsonSerialize(this.dependenciesInjection.applicationDetail))
+    }
+
+    public async publishNewConnectionPoolMember() {
+        console.log('[', 'publish', RedisEmittion.INITIALIZE_CONNECTION_POOL_MEMBER, ']', this.dependenciesInjection.applicationDetail);
+        await this.publisher?.publish(RedisEmittion.INITIALIZE_CONNECTION_POOL_MEMBER, 'hello');
+    };
+
+    public async initialize() {
+        this.subscriber = this.connection.duplicate()
+        this.publisher = this.subscriber.duplicate()
+        await this.subscriber.connect();
+        await this.publisher.connect();
+        this.subscriber.on(RedisEmittion.LISTEN_MESSAGE, InitializePool);
+        this.subscriber.on(RedisEmittion.INITIALIZE_CONNECTION_POOL_MEMBER, message => {
+            console.log('[', RedisEmittion.INITIALIZE_CONNECTION_POOL_MEMBER, ']:', this.jsonSerialize(message));
+        });
+        await this.subscriber.subscribe(RedisEmittion.INITIALIZE_CONNECTION_POOL_MEMBER, message => {
+            console.log('[', RedisEmittion.INITIALIZE_CONNECTION_POOL_MEMBER, ']:', this.jsonSerialize(message));
+        });
+
+        // await this.broadcastingNewApplicationInstanceMember();
     };
 };
 
-
 const redisConnection: any = /* appsettings.redisConnection */ {
-    host: Config.REDIS_HOST,
-    port: Config.REDIS_PORT
+    url: ['redis://', Config.REDIS_HOST.toString(), ':', Config.REDIS_PORT.toString()].join(''),
+
 } as any;
 
 
