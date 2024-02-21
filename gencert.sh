2de18fe371a411fc14939ea4e236e15e7fd58aff
contain3rlab <svnps.rnd.team@gmail.com>
2024-02-21 13:56:07 +0700

    apply fix wrong typo

--- gencert.sh
+++ gencert.sh
@@ -4,5 +4,4 @@ openssl req -newkey rsa:2048 -nodes -keyout ./selfsign-ssl/privatekey.pem -x509 
 -addext "subjectAltName=DNS:*.hyperscaleapplicationshowcase.com" \
 -addext "basicConstraints=critical,CA:TRUE,pathlen:0" \
 -addext "keyUsage=critical,keyCertSign,cRLSign,digitalSignature"
-
-openssl verify -CAfile ./selfsign-ssl/certificate.pem - verify_hostname '*.hyperscaleapplicationshowcase.com' ./selfsign-ssl/certificate.pem
+openssl verify -CAfile ./selfsign-ssl/certificate.pem -verify_hostname '*.hyperscaleapplicationshowcase.com' ./selfsign-ssl/certificate.pem
