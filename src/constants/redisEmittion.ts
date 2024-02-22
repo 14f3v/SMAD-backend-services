d5169123495f21c2fa55ac6c6f8e340d2c928fb1
contain3rlab <svnps.rnd.team@gmail.com>
2024-02-22 16:27:22 +0700

    Applied Description
    - applied a description for event listener on each constant emittion values for both of Socket emittion and redis

--- src/constants/redisEmittion.ts
+++ src/constants/redisEmittion.ts
@@ -1,6 +1,9 @@
+// ! Lowercase letter mean an event's constant of library implemented
+// ? Capital mean our implementation
+
 enum RedisEmittion {
     LISTEN_MESSAGE = 'message',
-    INITIALIZE_CONNECTION_POOL_MEMBER = 'initialize_connection_pool_member'
+    INITIALIZE_CONNECTION_POOL_MEMBER = 'INITIALIZE_CONNECTION_POOL_MEMBER'
 };
 
 export default RedisEmittion;
