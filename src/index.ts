b1403000ff1790cf6b15bc46f943e899c38097eb
contain3rlab <svnps.rnd.team@gmail.com>
2024-02-23 13:23:28 +0700

    force commit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
+import '../pre-start';
 import Config from '@constants/configs';
 import SocketIOEmittion from '@constants/socketIOemittion';
 import { socketIO, server, app } from '@server';
-import RedisServices from '@services/redis';
 import DependenciesInjection from '@services/dependenciesInjection';
 import SocketIOService from '@services/socketIOService';
 
-const redisServices = RedisServices.getInstance();
 export default (async () => {
     server.listen(Config.APPLICATION_PORT, new DependenciesInjection().initInstance);
     socketIO.on(SocketIOEmittion.CONNECTION, (socket) => new SocketIOService(socket));
-    await redisServices.initialize().then(async (ref) => await redisServices.broadcastingNewApplicationInstanceMember());
+    // socketIO.sockets.on(SocketIOEmittion.CONNECTION, (socket) => new SocketIOService(socket));
     return;
 })();
