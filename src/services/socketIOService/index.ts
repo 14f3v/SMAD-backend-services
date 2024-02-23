c52758dda9bff410e4a1c4150fefb0d19883c0de
contain3rlab <svnps.rnd.team@gmail.com>
2024-02-23 18:09:26 +0700

    complete first mil stone for basic implementation

--- src/services/socketIOService/index.ts
+++ src/services/socketIOService/index.ts
@@ -4,18 +4,18 @@ import SocketIOEmittion from '@constants/socketIOemittion';
 import { hostname } from 'os';
 import Repositories, { Message } from '@repos/index';
 import RedisServices from '@services/redis';
-// let connectionPools: ConnectionPool[] = [];
+
+let countSim = 1;
 
 class WebsocketConnectionPool {
     private static instance: WebsocketConnectionPool;
-    public pools: WebSocketConnectionPool[] = [];
+    public pools: ConnectionPool[] = [];
     public static getInstance(): WebsocketConnectionPool {
         if (!WebsocketConnectionPool.instance) {
             WebsocketConnectionPool.instance = new WebsocketConnectionPool();
         }
         return WebsocketConnectionPool.instance;
     };
-
 };
 
 export default class SocketIOService {
@@ -58,11 +58,49 @@ export default class SocketIOService {
         return conext;
     }
 
+    private handlingInterNodeMessaging<T = undefined>(message: T | any) {
+        // ? { username // destination username, socketId destination socketId, message } from source
+        const senderUser = this.clientConnection.pools.find(({ socketId }) => socketId == this._socketInstance.id);
+        const destinationReciver = this.clientConnection.pools.find(({ username, socketId }) => username == message.username && socketId == message.socketId);
+        const validMessage = new Message();
+        validMessage.message = message.message;
+        validMessage.to_username = message.username;
+
+        if (senderUser) {
+            const { username: senderUsername } = senderUser;
+            validMessage.username = senderUsername;
+        }
+
+        if (destinationReciver && (countSim > 1)) {
+            // ? ----------
+            // ? condition of countSim > 1 is for simulate that the inter node didn't hold a first connection, 
+            // ? then have to publish to other inter node pools, and inter node in pools which have a connection pools of first emission even, 
+            // ? then let that node emit a message
+            // ? this condition can be remove if you run a whole stack, and don't have to simulate this condition
+            // ? ----------
+
+            const { socketId } = destinationReciver;
+            message.username = senderUser?.username!;
+            message.socketId = senderUser?.socketId!;
+            console.log('[', 'EMIT DIRECT MESSAGE', ']', validMessage, '\n');
+            this._socketInstance.to(socketId).emit(SocketIOEmittion.CHAT_MESSAGE, message);
+            this.repositories.postMessage(message);
+        }
+
+        else {
+            // ? implement publishing message via redis pub/sub paradigm here!!
+            console.log('[', 'PUBLISH MESSAGE THROUGHT OUT', ']', validMessage, '\n');
+            countSim = countSim + 1;
+            this.redisServices.publishInterNodeMeesageMessage(SocketIOEmittion.CHAT_MESSAGE, message);
+        }
+    };
+
+
     /**
      * broadcasting
      */
     public broadcastingUsersOnline() {
-        console.log('broadcastingUsersOnline', this.clientConnection.pools.map(({ socket, ...element }) => element))
+        console.log('broadcastingUsersOnline', this.clientConnection.pools.map(({ ...element }) => element))
         // console.log('broadcastingUsersOnline connectionPools', connectionPools.map(({ ...element }) => element))
         this.messageEmittion = new MessageEmittion();
         this.messageEmittion.hostname = hostname();
@@ -72,30 +110,24 @@ export default class SocketIOService {
         this._socketInstance.broadcast.emit(SocketIOEmittion.USERS_ONLINE, this.messageEmittion); // ? broadcast emittion to another socket
     };
 
+
     /**
      * listenOnNewUserRegistered
      */
     public listenOnNewUserRegistered() {
         this._socketInstance.on(SocketIOEmittion.USER_REGISTERED, (io: ConnectionPool) => {
             console.log('[ ' + SocketIOEmittion.USER_REGISTERED + ' ]: ' + this.jsonSerialize(io));
-            const webSocketConnectionPool = new WebSocketConnectionPool();
-            /* 
-            const newConnection = new ConnectionPool();
-            newConnection.socketId = this._socketInstance.id;
-            newConnection.username = io.username;
-            newConnection.hostname = hostname(); 
-            */
+            const webSocketConnectionPool = new ConnectionPool();
             webSocketConnectionPool.socketId = this._socketInstance.id;
             webSocketConnectionPool.username = io.username;
             webSocketConnectionPool.hostname = hostname();
-            // webSocketConnectionPool.socket = this._socketInstance;
             this.clientConnection.pools = [...this.clientConnection.pools ?? [], webSocketConnectionPool]; // ? TODO: this should be turn into a complete datbase integration in session table
-            // connectionPools = [...connectionPools ?? [], webSocketConnectionPool]; // ? TODO: this should be turn into a complete datbase integration in session table
             /* //? fact: better store a connection pool member into redis[EX] */
-            this.repositories.addUser(/* newConnection */webSocketConnectionPool);
+            this.repositories.addUser(webSocketConnectionPool);
             this.broadcastingUsersOnline();
         });
     };
+
 
     /**
      * listenOnDisconnect
@@ -107,39 +139,22 @@ export default class SocketIOService {
         });
     };
 
+
     /**
      * messageChat
      */
     public listenOnChatMessage() {
-        this._socketInstance.on(SocketIOEmittion.CHAT_MESSAGE, (message) => {
-            // ? { username // destination username, socketId destination socketId, message } from source
-            const senderUser = this.clientConnection.pools.find(({ socketId }) => socketId == this._socketInstance.id);
-            const destinationReciver = this.clientConnection.pools.find(({ username, socketId }) => username == message.username && socketId == message.socketId);
-            const validMessage = new Message();
-            validMessage.message = message.message;
-            validMessage.to_username = message.username;
-
-            if (senderUser) {
-                const { username: senderUsername } = senderUser;
-                validMessage.username = senderUsername;
-            }
-
-            if (destinationReciver) {
-                const { socketId } = destinationReciver;
-                message.username = senderUser?.username!;
-                message.socketId = senderUser?.socketId!;
-                console.log('[', 'EMIT DIRECT MESSAGE', ']', validMessage, '\n');
-                this._socketInstance.to(socketId).emit(SocketIOEmittion.CHAT_MESSAGE, message);
-                this.repositories.postMessage(message);
-            }
-
-            else {
-                // ? implement publishing message via redis pub/sub paradigm here!!
-                console.log('[', 'PUBLISH MESSAGE THROUGHT OUT', ']', validMessage, '\n');
-            }
-
-        });
+        this._socketInstance.on(SocketIOEmittion.CHAT_MESSAGE, (message) => this.handlingInterNodeMessaging(message));
     };
+
+
+    /**
+     * websocketInterNodeEmission
+     */
+    public websocketInterNodeEmission(socketIOemittion: SocketIOEmittion, message: any) {
+        this.redisServices.listenOnInterNodeMeesageMessage(this.handlingInterNodeMessaging);
+    };
+
 
     /**
      * listenChatHistories
@@ -193,18 +208,3 @@ class MessageEmittion<T> implements TMessageEmittion<T> {
 };
 
 interface IMessageEmittion<T = undefined> extends MessageEmittion<T> { };
-
-class WebSocketConnectionPool extends ConnectionPool {
-    socket: Socket;
-    constructor(
-        username?: string,
-        socketId?: string,
-        hostname?: string,
-        socket?: Socket,
-    ) {
-        super(username, socketId, hostname);
-        this.socket = socket!;
-    }
-};
-
-interface IWebSocketConnectionPool extends WebSocketConnectionPool { }
